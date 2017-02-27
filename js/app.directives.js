63ff04ae5f9dfe53e5eb52d09991a64cb1ea834e
Andrea Leone <andrea@etrentino.it>
2017-02-28 00:50:50 +0100

    0.0.15
    Create a page for each city

--- js/app.directives.js
+++ js/app.directives.js
@@ -17,6 +17,48 @@
       };
     })
 
+    /// viewHome
+
+    .directive('viewTrento', function(){
+      return{
+        restrict: 'AE',
+        scope: {
+          key: '='
+        },
+        replace: false,
+        controller: 'cntrl_viewTrento',
+        templateUrl: '../html/view.trento.html'
+      };
+    })
+
+    /// viewHome
+
+    .directive('viewRovereto', function(){
+      return{
+        restrict: 'AE',
+        scope: {
+          key: '='
+        },
+        replace: false,
+        controller: 'cntrl_viewRovereto',
+        templateUrl: '../html/view.rovereto.html'
+      };
+    })
+
+    /// viewHome
+
+    .directive('viewPergine', function(){
+      return{
+        restrict: 'AE',
+        scope: {
+          key: '='
+        },
+        replace: false,
+        controller: 'cntrl_viewPergine',
+        templateUrl: '../html/view.pergine.html'
+      };
+    })
+
     ;///
 
 }());
