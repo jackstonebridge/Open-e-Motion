b2fc262ddf0f51c2dad2e8c9f83ab135cc2db082
Andrea Leone <andrea@etrentino.it>
2017-02-27 19:16:47 +0100

    0.0.6

--- js/functions.js
+++ js/functions.js
@@ -1,47 +1,28 @@
 $(document).ready(function(){
 
-  var location = "Rovereto";
+  $( "#input" ).bind("keyup", function(event) {
 
-  if(location == "Trento"){
+    var val = event.target.value;
 
-    $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/trento', function(data) {
-
-      $.each( data, function(i, val){
-
-        console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots)
-
+    if(event.target.value == "Trento"){
+      $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/trento', function(data) {
+        $.each( data, function(i, val){
+          console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots);
+        });
       });
-
-    });
-
-  } else if(location == "Rovereto"){
-
-    $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/rovereto', function(data) {
-
-      $.each( data, function(i, val){
-
-        console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots)
-
+    } else if(event.target.value == "Rovereto"){
+      $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/rovereto', function(data) {
+        $.each( data, function(i, val){
+          console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots);
+        });
       });
-
-    });
-
-  } else if(location == "Pergine Valsugana"){
-
-    $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/pergine_valsugana', function(data) {
-
-      $.each( data, function(i, val){
-
-        console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots)
-
+    } else if(event.target.value == "Pergine Valsugana"){
+      $.getJSON('https://os.smartcommunitylab.it/core.mobility/bikesharing/pergine_valsugana', function(data) {
+        $.each( data, function(i, val){
+          console.log(val.name + " –– " + val.bikes + ":" + val.slots + "/" + val.totalSlots);
+        });
       });
-
-    });
-
-  };
-
-  function click(){
-    console.log("Works!")
-  }
+    };
+  });
 
 });
