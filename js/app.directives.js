c1cf2a02da90e079ffc428c065d8116543a51e4b
Andrea Leone <andrea@etrentino.it>
2017-02-27 20:02:43 +0100

    0.0.11
    Add integration with Angular & Angular-UI (router)

---
+++ js/app.directives.js
@@ -0,0 +1,22 @@
+(function() {
+
+  angular
+    .module('cnt')
+
+    /// viewHome
+
+    .directive('viewHome', function(){
+      return{
+        restrict: 'AE',
+        scope: {
+          key: '='
+        },
+        replace: false,
+        controller: 'cntrl_viewHome',
+        templateUrl: '../html/view-home.html'
+      };
+    })
+
+    ;///
+
+}());
